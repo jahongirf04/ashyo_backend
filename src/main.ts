024913cadf2a0ffd98bc98983614d079c70d9058
Dilnoza Imomova <imomova06@gmail.com>
2023-11-10 01:11:55 +0500

    swagger, pg and sequelize packages are added, app.module configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start() {
+  try {
+    const config = new DocumentBuilder()
+      .setTitle('Ashyo OnlineShop Project')
+      .setDescription('Mini project for Ashyo OnlineShop')
+      .setVersion('1.0.0') 
+      .addTag('NodeJs, NestJs, Postgres, Sequelize') 
+      .build();
+
+    const PORT = process.env.PORT || 3003;
+    const app = await NestFactory.create(AppModule, {
+      logger: ['error', 'warn', 'log'],
+    });
+    app.setGlobalPrefix('api');
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
+    app.use(cookieParser());
+    app.useGlobalPipes(new ValidationPipe());
+
+    await app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
 }
-bootstrap();
+
+start();
